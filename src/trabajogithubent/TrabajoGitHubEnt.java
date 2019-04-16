ac56dbc37e26dd4cc10ea451489c04c43f1a2678
Jonatan <Jonatan@Benjamin>
2019-04-16 18:54:41 +0200

    1º versión trabajo entornos GitHub

---
+++ src/trabajogithubent/TrabajoGitHubEnt.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajogithubent;
+
+/**
+ *
+ * @author Jonatan y David
+ */
+public class TrabajoGitHubEnt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1=6;
+        int n2=9;
+        int resul;
+        
+        resul=n1+n2;
+        
+        System.out.print("La suma de " + n1 + " + " + n2 + " = " + resul);
+                
+                }
+}
